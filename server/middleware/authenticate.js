62d217818ce87b7010c55fa6d05e00c3bb791cfa
Akhil <akhil.130241@gmail.com>
2024-08-29 01:39:46 +0530

    https true 45

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -4,8 +4,8 @@ const User = require("../model/schema").User;
 const Authenticate = async (req, res, next) => {
     try{
         const token = req.cookies.jwtoken;
-        // console.log("token is :", token)
-        // console.log("secret key is: ",process.env.SECRET_KEY );
+        console.log("token is :", token)
+        console.log("secret key is: ",process.env.SECRET_KEY );
         if(!token){
             console.log("unauthorised: user not logged in");
             return res.status(401).send("unauthorised: user not logged in");
